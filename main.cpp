c03dbe0ba3dbde528794cb411963e2eb831a42e4
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-04-04 22:14:47 +0800

    Add vertex attribute

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		std::cout << "Escape pressed" << std::endl;
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	std::cout << "Width " << width << " Height " << height << std::endl;
-	glViewport(0, 0, width, height);
+	//glViewport(0, 0, width, height);
 }
 
 int main()
@@ -32,7 +40,30 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		processInput(window);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
